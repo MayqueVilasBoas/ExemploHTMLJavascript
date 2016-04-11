81f17a57ee38f8ea2ace6b1ce4763ff941906a83
Mayque <mayque@MacBook-Air-de-Mayque.local>
2016-04-10 22:55:21 -0300

    exemplo de utilização de Javascript em paginas HTML

---
+++ js/funcoes.js
@@ -0,0 +1,15 @@
+function clickMensagem(){
+    alert("Mostrando mensagem");
+}
+
+function clickCampo(){
+
+    var str = document.getElementById("cNome").value;
+
+    alert(str);
+}
+
+function clickFocar(){
+
+    document.getElementById("cNome").focus();
+}
